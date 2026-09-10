95a7b3e033d7dd4c3369e479570786095ec043a4
francescabarbieri-commits <barbierifrancesca216@gmail.com>
2026-09-10 19:32:35 -0300

    Add files via upload

---
+++ funciones_matematicas.php
@@ -0,0 +1,26 @@
+<?php
+
+//ej 1
+echo pow(5,3);
+echo "<br>";
+
+//ej 2
+echo sqrt(9);
+echo "<br>";
+
+//ej 3
+echo rand(1, 100);
+echo "<br>";
+
+//ej 4
+echo pi();
+echo "<br>";
+
+//ej 5
+echo floor(4.3);
+echo "<br>";
+echo ceil(4.3);
+echo "<br>";
+
+//ej 6
+echo round(3.5);
