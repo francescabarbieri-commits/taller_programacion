963c1a31df5a1586e74bf8bfdb06f11843c87c9d
francescabarbieri-commits <barbierifrancesca216@gmail.com>
2026-09-10 13:15:34 -0300

    Add files via upload

---
+++ funciones_string.php
@@ -0,0 +1,36 @@
+<?php
+
+//ejercicio 1
+
+$cadena_texto= "Hola Mundo";
+
+echo strtolower ($cadena_texto);
+
+
+// ejercicio 2
+
+$cadena_texto= "Hola Mundo"."<br>";
+$cadena_texto=strtolower ($cadena_texto);
+
+echo $cadena_texto; 
+
+// ejercicio 3
+
+$cadena_texto= "Hola Mundo"."<br>";
+$cadena_texto=strtoupper ($cadena_texto);
+
+echo $cadena_texto; 
+
+// ejercicio 4
+
+$cadena_texto= "Hola Mundo"."<br>";
+$cadena_texto=ucfirst ($cadena_texto);
+
+echo $cadena_texto; 
+
+// ejercicio 5
+
+$cadena_texto= "Hola Mundo"."<br>";
+$cadena_texto=ucwords ($cadena_texto);
+
+echo $cadena_texto; 
