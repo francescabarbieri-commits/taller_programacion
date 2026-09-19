ef2c524d18ee725a7d37ff7c0bcb8fb40a6e4220
francescabarbieri-commits <barbierifrancesca216@gmail.com>
2026-09-19 20:39:05 -0300

    Add files via upload

---
+++ form_index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nombre=$_POST["nombre"];
+
+echo $nombre;
+
+$asignatura= $_POST["asignatura"];
+
+$frutas= $_POST["frutas"];
+
+echo $nombre." - ".$asignatura." - ".$frutas;
+
+
+
+
---
+++ formulario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="form_index.php" method= "POST"> 
+    <div>
+        <label for= "nombre" > Nombre</label>
+        <input type="text" id="nombre" name="nombre">
+    </div>
+
+    <br>
+
+    <label for="asignatura">Asignatura</label>
+    <select id="asignatura" name="asignatura" >
+        <option value="Ingles">Ingles</option>
+        <option value="Matematicas">Matemáticas</option>
+        <option value="Ciencia">Ciencia</option>
+        <option value="Lenguaje">Lenguaje</option>
+
+    </select>
+
+    <br><br>
+
+    <label for="opcion-1">
+        <input type= "checkbox" value= "Manzana" id="opcion-1" name= "frutas">
+        Manzana
+    </label>
+
+
+    <br><br><br>
+
+        <button type= "submit" > Enviar </button>
+
+    </form>
+
+</body>
+</html>
